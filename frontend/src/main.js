a5d3b04ad9072cb56b285293d3144fe115c868d0
atesanilerdem <atesanilerdem@gmail.com>
2021-12-04 20:52:10 +0300

    Add .env configs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+
 /* Moralis init code */
-const serverUrl = "https://kmevmlphg8rq.usemoralis.com:2053/server";
-const appId = "lOhK4PrszxppOZxHOZwu1gKfb22o4kI9gssLUftO";
+const serverUrl = process.env.VUE_APP_SERVER_URL
+const appId = process.env.VUE_APP_APP_ID
 Moralis.start({ serverUrl, appId });
 
 
