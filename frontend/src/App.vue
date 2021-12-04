67212d123c19c5d7fb1afe5c4969fa272f2a4c87
atesanilerdem <atesanilerdem@gmail.com>
2021-12-04 15:57:02 +0300

    Fix a git issue

---
+++ frontend/src/App.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+#root
+  app-navbar
+</template>
+
+<script>
+import Navbar from "./components/shared/Navbar";
+
+export default {
+  components: {
+    appNavbar: Navbar,
+  } 
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+
+  font-family: "Poppins", sans-serif;
+  box-sizing: border-box;
+}
+
+/******** Flex Classes ********/
+
+.flex {
+  display: flex;
+
+  &-row {
+    @extend .flex;
+    flex-direction: row;
+  }
+
+  &-col {
+    @extend .flex;
+    flex-direction: column;
+  }
+
+  &-jc-c {
+    justify-content: center;
+  }
+
+  &-jc-sa {
+    justify-content: space-around;
+  }
+
+  &-jc-sb {
+    justify-content: space-between;
+  }
+
+  &-ai-c {
+    align-items: center;
+  }
+
+  &-ai-fs {
+    align-items: flex-start;
+  }
+}
+</style>
---
+++ frontend/src/components/shared/Navbar.vue
@@ -0,0 +1,83 @@
+<template lang="pug">
+nav.navbar
+  .navbar-left.flex-row.flex-ai-c.flex-jc-sa
+    router-link(to="/") LOGO IS HERE
+  .navbar-middle
+    | Search Bar Is Here
+  .navbar-items.flex-col.flex-jc-c
+    ul.flex-row.flex-jc-sa
+      li
+        a Explore
+      li(@click="loginx")
+        a Sign In
+      li(@click="logoutX")
+        a Sign Out
+      li
+        a Profile
+      router-link(to="/settings", tag="li")
+        a Settings
+</template>
+
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  color: white;
+  height: 4.6rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  box-shadow: 0 1px 10px gray;
+
+  &-left {
+    color: black;
+    width: 24rem;
+
+    list-style-type: none;
+
+    font-weight: 600;
+    font-size: 1.5rem;
+
+    box-sizing: border-box;
+    padding-right: 8rem;
+  }
+
+  &-middle {
+    width: 36rem;
+  }
+
+  &-items {
+    width: 36rem;
+  }
+}
+
+ul {
+  list-style-type: none;
+
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  line-height: 1.5rem;
+
+  cursor: pointer;
+  /* Make text not selectable */
+  -webkit-user-select: none; /* Chrome all / Safari all */
+  -moz-user-select: none; /* Firefox all */
+  -ms-user-select: none; /* IE 10+ */
+  user-select: none; /* Likely future */
+}
+
+a {
+  text-decoration: none;
+  color: gray;
+}
+</style>
