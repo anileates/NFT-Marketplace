966734046dd7bdfe88b0f8dece0d09a0c7ddaf76
atesanilerdem <atesanilerdem@gmail.com>
2021-12-30 16:46:13 +0300

    Make wallet-adress copiable

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -7,68 +7,89 @@
       .information-section.flex-col.flex-ai-fs.flex-jc-sa
         .info.flex-col.flex-ai-fs.flex-jc-sa
           label Username*
-          appInputBox(id="username" :value="getCurrentUser.username")
+          appInputBox#username(:value="getCurrentUser.username")
         .info.flex-col.flex-ai-fs.flex-jc-sa
           label Bio
-          appInputBox(id="biography" :value="getCurrentUser.biography")
+          appInputBox#biography(:value="getCurrentUser.biography")
         .info.flex-col.flex-ai-fs.flex-jc-sa
           label Email address
-          appInputBox(id="email" :value="getCurrentUser.email")
+          appInputBox#email(:value="getCurrentUser.email")
         .info.flex-col.flex-ai-fs.flex-jc-sa
           label Wallet address
-          appInputBox(id="wallet-input" type="text" :value="getCurrentUser.ethAddress" :isReadOnly="true")
+          .flex-row.flex-ai-c
+            appInputBox#wallet-input(
+              type="text",
+              :value="getCurrentUser.ethAddress",
+              :isReadOnly="true"
+            )
+            i.far.fa-clipboard.fa-1x(
+              style="position: absolute; margin-left: 24.5rem; cursor: pointer",
+              @click="copyWalletAdress"
+            )
         button(@click="saveUserInfo") Save
       .image-section.flex-col.flex-ai-fs.flex-jc-sa
         .section.flex-col.flex-ai-fs.flex-jc-sa
           label Profile Image
           .image-box-profile(style="border-radius: 50%")
-            app-avatar(:imgUrl="getCurrentUser.avatar && getCurrentUser.avatar._url" @uploaded="changeAvatar")
+            app-avatar(
+              :imgUrl="getCurrentUser.avatar && getCurrentUser.avatar._url",
+              @uploaded="changeAvatar"
+            )
 
         .section.flex-col.flex-ai-fs.flex-jc-sa
           label Cover Image
           .image-box-cover(style="border-radius: 5%")
-            app-avatar(:imgUrl="getCurrentUser.cover && getCurrentUser.cover._url" @uploaded="changeCover")
+            app-avatar(
+              :imgUrl="getCurrentUser.cover && getCurrentUser.cover._url",
+              @uploaded="changeCover"
+            )
 </template>
 
 <script>
-import {mapGetters, mapActions} from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 import Avatar from "./shared/Avatar";
-import InputBox from './shared/InputBox'
+import InputBox from "./shared/InputBox";
 
 export default {
   name: "Settings",
   components: {
     appAvatar: Avatar,
-    appInputBox: InputBox
+    appInputBox: InputBox,
   },
   data() {
     return {
-      updatedUser: {}
+      updatedUser: {},
     };
   },
   computed: {
-    ...mapGetters(['getCurrentUser'])
+    ...mapGetters(["getCurrentUser"]),
   },
   methods: {
-    ...mapActions(['updateUser']),
+    ...mapActions(["updateUser"]),
     async saveUserInfo() {
-      this.updatedUser.username = document.getElementById('username').value
-      this.updatedUser.biography = document.getElementById('biography').value
-      this.updatedUser.email = document.getElementById('email').value
-      this.updatedUser.ethAddress = document.getElementById('wallet-input').value
+      this.updatedUser.username = document.getElementById("username").value;
+      this.updatedUser.biography = document.getElementById("biography").value;
+      this.updatedUser.email = document.getElementById("email").value;
+      this.updatedUser.ethAddress =
+        document.getElementById("wallet-input").value;
 
-      await this.updateUser(this.updatedUser)
-      alert('Informations has been changed') // TODO will be changed with SweetAlert
+      await this.updateUser(this.updatedUser);
+      alert("Informations has been changed"); // TODO will be changed with SweetAlert
     },
-    changeAvatar(file){
-      const moralisFile = new Moralis.File('avatar.jpg', file)
-      this.updateUser({ avatar: moralisFile })
+    changeAvatar(file) {
+      const moralisFile = new Moralis.File("avatar.jpg", file);
+      this.updateUser({ avatar: moralisFile });
     },
     changeCover(file) {
-      const moralisFile = new Moralis.File('cover.jpg', file);
-      this.updateUser({ cover: moralisFile })
-    }
-  }
+      const moralisFile = new Moralis.File("cover.jpg", file);
+      this.updateUser({ cover: moralisFile });
+    },
+    copyWalletAdress() {
+      let copyText = document.getElementById("wallet-input");
+      copyText.select();
+      navigator.clipboard.writeText(copyText.value);
+    },
+  },
 };
 </script>
 
