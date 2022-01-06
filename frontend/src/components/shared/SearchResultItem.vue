77faa7fac66bfb9e177ccfd23452149a75987aee
atesanilerdem <atesanilerdem@gmail.com>
2022-01-06 23:51:39 +0300

    Update searchResultItem component

--- frontend/src/components/shared/SearchResultItem.vue
+++ frontend/src/components/shared/SearchResultItem.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-a.search-result-item.flex-row.flex-ai-c
+a.search-result-item.flex-row.flex-ai-c(:href="getRedirectUrl")
   .avatar-wrapper
-    app-avatar(:imgUrl="resultItem.attributes.avatar._url" :isEditable="false")
-  p {{ resultItem.attributes.username }}
+    app-avatar(:imgUrl="getGivenItem.imageUrl" :isEditable="false")
+  p {{ getGivenItem.title }}
+  span.itemType - {{ getGivenItem.itemType }}
 </template>
 
 <script>
@@ -11,13 +12,86 @@ import Avatar from "./Avatar";
 export default {
   name: "SearchResultItem",
   props: {
-    resultItem: {}
+    userItem: {
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      title: { // title also stands for username here
+        type: String,
+        required: true
+      },
+    },
+    collectionItem: {
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      contractAddress: {
+        type: String,
+        required: true
+      }
+    },
+    assetItem: {
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      contractAddress: {
+        type: String,
+        required: true
+      },
+      tokenId: {
+        type: String,
+        required: true
+      }
+    }
   },
   components: {
     appAvatar: Avatar
   },
   data() {
-    return {
+    return {}
+  },
+  computed: {
+    getGivenItem() {
+      if (this.assetItem) {
+        return {
+          imageUrl: this.assetItem.imageUrl,
+          title: this.assetItem.title,
+          itemType: 'asset'
+        }
+      } else if (this.userItem) {
+        return {
+          imageUrl: this.userItem.imageUrl,
+          title: this.userItem.title,
+          itemType: 'profile'
+        }
+      } else if (this.collectionItem) {
+        return {
+          imageUrl: this.collectionItem.imageUrl,
+          title: this.collectionItem.title,
+          itemType: 'collection'
+        }
+      }
+
+    },
+    getRedirectUrl() {
+      if (this.userItem) {
+        return `/users/${this.userItem.title}`
+      } else if (this.assetItem) {
+        return `/tokens/${this.assetItem.contractAddress}/${this.assetItem.tokenId}`
+      } else if (this.collectionItem) {
+        return `/collections/${this.collectionItem.contractAddress}`
+      }
     }
   }
 }
@@ -25,6 +99,7 @@ export default {
 
 <style lang="scss">
 a {
+  text-decoration: none;
   /* Make text not selectable */
   -webkit-user-select: none; /* Chrome all / Safari all */
   -moz-user-select: none; /* Firefox all */
@@ -35,11 +110,16 @@ a {
 .search-result-item {
   width: 100%;
   height: 100%;
-  padding-left: 1rem;
+  padding: 0 1rem;
 
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  //width:300px; // some width
   color: black;
 
   cursor: pointer;
+
   &:hover {
     box-shadow: 2px 2px 30px 0 rgba(197, 197, 197, .9)
   }
@@ -55,4 +135,13 @@ a {
   }
 }
 
+
+.itemType {
+  font-size: 0.8rem;
+  margin-left: 0.5rem;
+  margin-top: 0.1rem;
+
+  letter-spacing: 0.03rem;
+  color: gray
+}
 </style>
