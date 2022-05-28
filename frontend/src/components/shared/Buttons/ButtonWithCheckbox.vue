2501d3f0613273ccae16de7b2bf75ccf0b6eb954
anileates <atesanilerdem@gmail.com>
2022-05-28 17:07:34 +0300

    Add "Custom checkbox & CustomButton"

---
+++ frontend/src/components/shared/Buttons/ButtonWithCheckbox.vue
@@ -0,0 +1,69 @@
+<script>
+/**
+ * A buttton with checkbox. When button clicked checkbox gets toggled.
+ * Mainly, will be used for filters
+ */
+
+import CustomCheckbox from "../Buttons/CustomCheckbox.vue";
+
+export default {
+  name: "ButtonWithCheckbox",
+  components: {
+    appCustomCheckbox: CustomCheckbox
+  },
+  props: {
+    buttonText: {
+      type: String,
+      required: true,
+      default: "No text",
+    },
+  },
+  data() {
+    return {
+      isClicked: false
+    }
+  },
+  methods: {
+    buttonClicked() {
+      this.isClicked = !this.isClicked;
+      this.$emit("buttonCboxClicked");
+    }
+  }
+};
+</script>
+
+<template lang="pug">
+button.flex__row.flex__jc-sb.flex__ai-c(@click="buttonClicked")
+  p {{ buttonText }}
+  .customCheckboxWrapper
+    appCustomCheckbox(:checked="isClicked" )
+</template>
+
+
+<style lang="scss" scoped>
+button {
+  background-color: transparent;
+  padding: 12px 16px;
+  width: 100%;
+  border: none;
+  border-radius: 10px;
+
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 0.8px;
+
+  transition: 0.1s;
+  &:focus,
+  &:hover {
+    background-color: rgba(229, 232, 235, 0.5);
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+}
+
+.customCheckboxWrapper {
+  width: 30px;
+  height: 30px;
+}
+</style>
---
+++ frontend/src/components/shared/Buttons/CustomCheckbox.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+.round
+  input#checkbox(type="checkbox", :checked="checked")
+  label
+</template>
+
+<script>
+export default {
+  name: "CustomCheckbox",
+  props: {
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.round {
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+}
+
+.round label {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+
+  position: absolute;
+  left: 0;
+  top: 0;
+
+  height: 100%;
+  width: 100%;
+
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.round label:after {
+  border: 2px solid #fff;
+  border-top: none;
+  border-right: none;
+
+  opacity: 0;
+  content: "";
+
+  position: absolute;
+  transform: rotate(-45deg);
+  top: 32%;
+  left: 26%;
+
+  height: 20%;
+  width: 42%;
+}
+
+.round input[type="checkbox"] {
+  visibility: hidden;
+}
+
+.round input[type="checkbox"]:checked + label {
+  background-color: #66bb6a;
+  border-color: #66bb6a;
+}
+
+.round input[type="checkbox"]:checked + label:after {
+  opacity: 1;
+}
+</style>
