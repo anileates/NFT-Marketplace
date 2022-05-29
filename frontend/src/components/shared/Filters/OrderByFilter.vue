4447ead750d8e68180fcf79dead5275ef2add16f
anileates <atesanilerdem@gmail.com>
2022-05-29 17:49:30 +0300

    Add Order By Filter

---
+++ frontend/src/components/shared/Filters/OrderByFilter.vue
@@ -0,0 +1,30 @@
+<script>
+import Filter from "./Filter.vue";
+import ButtonWithCheckbox from "../Buttons/ButtonWithCheckbox.vue";
+
+export default {
+  name: "OrderByFilter",
+  components: {
+    appFilter: Filter,
+    appButtonWithCheckbox: ButtonWithCheckbox,
+  },
+};
+</script>
+
+<template lang="pug">
+appFilter(filterName="Order By")
+  template(v-slot:filter-content)
+    appButtonWithCheckbox.custom-font(
+      buttonText="Price: Low to High",
+    )
+    appButtonWithCheckbox.custom-font(
+      buttonText="Price: High to Low",
+    )
+</template>
+
+<style lang="scss" scoped>
+.custom-font {
+  font-weight: 400 !important;
+  letter-spacing: 0.2px;
+}
+</style>
